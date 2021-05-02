5d1a7b8919f6a497e04b9949ba375ee8e126e3f3
Julia Naomi <jnboeira@outlook.com>
2021-05-02 13:40:59 -0300

    leap

---
+++ leap/build/CMakeFiles/leap.dir/leap.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/leap.dir/leap.cpp.o: \
+  /Users/julianaomiboeira/Exercism/cpp/leap/leap.cpp \
+  /Users/julianaomiboeira/Exercism/cpp/leap/leap.h
---
+++ leap/leap.cpp
@@ -0,0 +1,8 @@
+#include "leap.h"
+
+namespace leap {
+    bool is_leap_year (unsigned int year) {
+        (void)year;
+        return false;
+    }
+}  // namespace leap
