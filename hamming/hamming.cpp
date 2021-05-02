b93a8cb0921a3cd38d0c7ed7c20dfa0c27469cfe
Julia Naomi <jnboeira@outlook.com>
2021-05-02 14:35:27 -0300

    returns-0

---
+++ hamming/hamming.cpp
@@ -0,0 +1,9 @@
+#include "hamming.h"
+#include <string>
+
+namespace hamming {
+    int compute(const std::string& a, const std::string& b) {
+        (void)a; (void)b;
+        return 0;
+    }
+}  // namespace hamming
