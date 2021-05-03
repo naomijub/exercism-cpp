c77d2a40cddda78089f6d0d36183c215dfc08ddb
Julia Naomi <jnboeira@outlook.com>
2021-05-03 09:24:47 -0300

    filter isogram with copy_if

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -6,9 +6,8 @@
 namespace isogram {
     bool is_isogram(std::string word) {
         std::string _word;
-        for (auto c : word) {
-            if (std::isalpha(c) != 0) _word += c;
-        } // replace by remove_if
+        auto is_alpha = [](char c) {return std::isalpha(c) != 0;};
+        std::copy_if(word.begin(), word.end(), back_inserter(_word), is_alpha);
         std::transform(_word.begin(), _word.end(), _word.begin(), ::tolower);
 
         return std::unordered_set<char>(_word.begin(), _word.end()).size() == _word.size();
